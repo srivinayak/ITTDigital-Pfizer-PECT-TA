1a193e220ba9760358fa13b692775a13a6235f94
Vinay Srinivasan <s.vinay.777@gmail.com>
2021-03-31 15:35:54 +0530

    myRunner.bat

--- ITTDigital-Pfizer-PECT-TA/run/myRunner.bat
+++ ITTDigital-Pfizer-PECT-TA/run/myRunner.bat
@@ -1,1 +1,13 @@
-    test_PECT_TC001.py
+cd ..
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC001.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC002.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC003.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC004.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC005.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC006.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC007.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC008.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC009.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC010.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC011.py
+pytest -vvv -s -n0 --screenshot=on --screenshot_path=./result/screenshot/ --alluredir=./result ./tests/steps/test_PECT_TC012.py
